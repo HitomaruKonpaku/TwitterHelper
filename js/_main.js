3a2aebe6a081ce07be1db7053bf9a311954361bf
HitomaruKonpaku <HitomaruKonpaku@users.noreply.github.com>
2017-10-10 18:41:43 +0700

    Add page and stream observer

--- js/_main.js
+++ js/_main.js
@@ -1,5 +1,49 @@
 console.info('TwitterHelper running...')
 
-$(document).ready(() => {
+let _obsPage, _obsStream
 
+$(document).ready(() => {
+    observePage()
 })
+
+// Check when page/url change
+function observePage() {
+    let target = $('#page-container').get(0)
+
+    _obsPage = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            console.log(mutation)
+
+            _obsStream.disconnect()
+            observeStream()
+        })
+    })
+
+    _obsPage.observe(target, { attributes: true })
+
+    observeStream()
+}
+
+// Check when load more tweets
+function observeStream() {
+    let target = $('#stream-items-id').get(0)
+
+    _obsStream = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            console.log(mutation)
+        })
+    })
+
+    _obsStream.observe(target, { childList: true })
+}
+
+// Add customize for every tweet
+function thTweetCustom() {
+
+}
+
+// <div class="stream-item-footer">
+function thStreamItemFooterAction() {
+    // return $('<div/>')
+    //     .addClass('')
+}
